8ec092de4db40de5821e6635601efacaba3f5b49
Matt Jaffee <jaffee@umbel.com>
2015-04-08 23:05:38 -0500

    trying to flesh out gosup - ran into a bug where the process isn't getting killed for some reason

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	fsnotify "gopkg.in/fsnotify.v1"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func check(err error) {
@@ -14,28 +16,54 @@ func check(err error) {
 
 func main() {
 	args := os.Args[1:]
+	chng := make(chan fsnotify.Op, 1)
+	go WatchFiles(args, chng)
 	for {
-		// TODO
+		kill := make(chan int, 1)
+		go StartProc(args, kill)
+		for (<-chng & fsnotify.Write) > 0 {
+		}
+		fmt.Println("Detected change... restarting")
+		kill <- 1
+		time.Sleep(time.Second * 2)
 	}
-	fmt.Println(args)
 }
 
-func StartProc(args []string) {
+func StartProc(args []string, kill chan int) {
+	fmt.Println("Making a proc")
 	cmd := exec.Command(args[0], args[1:]...)
-	stdout, err := cmd.StdoutPipe()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	err := cmd.Start()
 	check(err)
-	err = cmd.Start()
-	check(err)
-	a := make([]byte, 3)
-	n, err := stdout.Read(a)
-	fmt.Println(n)
-	fmt.Println(err)
-	fmt.Println(string(a))
-	err = cmd.Wait()
-	fmt.Println(err)
-
+	if 1 == <-kill {
+		fmt.Println("Killing process")
+		err := cmd.Process.Kill()
+		if err != nil {
+			fmt.Printf("Error killing process: %v\n", err)
+		}
+		if !cmd.ProcessState.Exited() {
+			fmt.Printf("Not exited? kill harder")
+			cmd.Process.Kill()
+		}
+	}
+	cmd.Wait()
 }
 
-func WatchFiles(args []string) {
-	// TODO
+func WatchFiles(args []string, chng chan fsnotify.Op) {
+	watcher, err := fsnotify.NewWatcher()
+	check(err)
+	watcher.Add(args[2])
+	fmt.Printf("watching %v\n", args[2])
+	defer watcher.Close()
+	for {
+		select {
+		case event := <-watcher.Events:
+			fmt.Printf("Watcher event: %v\n", event)
+			chng <- event.Op
+		case err := <-watcher.Errors:
+			check(err)
+		}
+	}
 }
